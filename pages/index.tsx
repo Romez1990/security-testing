8eb61a9320ce1cf0cb0cf4df915362464945b203
Romez <Romez1990@gmail.com>
2020-01-21 10:08:32 +0300

    feature(page): create index page

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+function IndexPage(): JSX.Element {
+  return (
+    <Fragment>
+      Стартовая страница
+    </Fragment>
+  );
+}
+
+export default IndexPage;
