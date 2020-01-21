56e59f4715173ef536b0dcaa521ebb4cf9b3587e
Romez <Romez1990@gmail.com>
2020-01-21 10:48:25 +0300

    feature(layout): create app bar

--- layouts/MainLayout.tsx
+++ layouts/MainLayout.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from 'react';
 import BaseLayout from './BaseLayout';
+import AppBar from '../components/layout/AppBar';
 
 interface Props {
   title?: string;
@@ -9,6 +10,7 @@ interface Props {
 function MainLayout({ title, children }: Props): JSX.Element {
   return (
     <BaseLayout title={title}>
+      <AppBar />
       {children}
     </BaseLayout>
   );
