5b86fa009a4ebd23692e4b9fda59e49667a819b7
Romez <Romez1990@gmail.com>
2020-02-13 07:17:53 +0300

    correct index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import makeStyles from '@material-ui/styles/makeStyles';
+import { Theme } from '@material-ui/core/styles';
+import createStyles from '@material-ui/styles/createStyles';
+import { Typography } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import MainLayout from '../layouts/MainLayout';
+
+const useStyles = makeStyles(({ spacing }: Theme) => createStyles({
+  button: {
+    marginTop: spacing(1),
+  },
+}));
 
 function IndexPage(): JSX.Element {
   const router = useRouter();
 
+  const classes = useStyles();
+
   return (
     <MainLayout>
-      Тестирование информационной безопасности
-      <br />
-      <br />
+      <Typography>Тестирование информационной безопасности</Typography>
       <Button
+        className={classes.button}
         variant="contained"
         color="primary"
         onClick={(): Promise<boolean> => router.push('/testing')}
